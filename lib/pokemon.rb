93c31821d81b9a89ddfbbd98fd568212b533bc2e
Jesus Vega <chuyspc@gmail.com>
2017-01-29 00:45:07 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,20 +2,24 @@ require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize(id: ,name:, type:, db: , hp = nil)
+  def initialize(id:, name:, type:, db:, hp: nil)
     @id = id
     @name = name
     @type = type
     @db = db
-    @hp = 60
+    @hp = hp || 60
   end
 
   def self.save(name,type,db)
-    db.prepare("INSERT INTO pokemon VALUES(?,?,?,?)").execute(@id,name,type,@hp)
+    db.prepare("INSERT INTO pokemon(name,type) VALUES(?,?)").execute(name,type)
   end
 
   def self.find(id,db)
     sample = db.execute("SELECT * FROM pokemon WHERE ID=?",id).flatten
-    self.new(id: 1, name: sample[1],type:  sample[2], db: db )
+    self.new(id: sample[0], name: sample[1],type:  sample[2], db: db , hp: sample[3])
+  end
+
+  def alter_hp(new_hp,db)
+    db.prepare("UPDATE pokemon SET hp=? WHERE id=?").execute(new_hp,self.id)
   end
 end
