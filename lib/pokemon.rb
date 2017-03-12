13682d8a48fc0035e8da41ab30ac1804b5c5b341
Matthew <github email address>
2017-03-13 00:56:44 +0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,14 +2,21 @@ require "pry"
 
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db
+	@@all = []
 
-  def initialize(id:, name:, type:, db:)
+  attr_accessor :id, :name, :type, :db, :hp
+
+  def initialize(id:, name:, type:, hp: nil, db:)
     @db = db
     @id = id
     @name = name
     @type = type
+    hp == nil ? @hp = 60 : @hp = hp
+    @@all << self
+  end
 
+  def self.all
+    @@all  	
   end
 
   def self.save(name, type, db)
@@ -18,6 +25,10 @@ class Pokemon
 
   def self.find(id, db)
   	pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
-  	Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+  	Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 end
