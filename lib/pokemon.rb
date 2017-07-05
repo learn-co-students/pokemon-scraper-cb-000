70a4ce038d9c47f2c061394ef09801ba26be5429
john vascsinec <pensteeler@gmail.com>
2017-07-05 20:12:40 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,12 +1,13 @@
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :id, :name, :type, :db, :hp
 
-  def initialize( id:, name:, type:, db: )
+  def initialize( id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
     @type = type
-    @db = db
+    #@db = db
+    @hp = hp
   end
 
   def self.save( name, type, db)
@@ -14,11 +15,23 @@ class Pokemon
   end
 
   def self.find( id, db )
-    results = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?", id ).flatten
-    p = Pokemon.new( id: results[0], name: results[1], type: results[2], db: db)
+    #results = db.execute("SELECT id, name, type FROM pokemon WHERE id = ?", [id] ).flatten
+    results = db.execute("SELECT * FROM pokemon WHERE id = ?", [id] ).flatten
+    Pokemon.new( id: results[0], name: results[1], type: results[2], hp: results[3], db: db)
+
+    #puts "P:#{p}"
     #puts "P-ID:#{p.id}"
     #puts "P-Name:#{p.name}"
-    p
+    #puts "P-Type:#{p.type}"
+    #puts "P-db:#{p.db}"
+    #puts "P-hp:#{p.hp}"
+    #p
+  end
+
+  def alter_hp( hp, db )
+    #expect(@db.execute("SELECT hp FROM pokemon").flatten.first).to eq(60)
+    puts "** Altering table for ID:#{self.id}, HP:#{hp}, and db:#{db}."
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [hp, self.id])
   end
 
 end
