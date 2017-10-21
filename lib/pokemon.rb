25ebf95d178ddd452ad235d38aa6bce579ae600a
Christina Anglin <anglin.christina@gmail.com>
2017-10-21 19:32:29 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,38 @@
 class Pokemon
+
+	attr_accessor :id, :hp
+	attr_reader :name, :type, :db
+
+
+	def initialize(name="No name", type="card", hp=nil, db=SQLite3::Database.new('test'))
+		@name = name
+		@type = type
+		@db = db
+		@hp = hp
+		#@hp = db.execute("SELECT hp FROM pokemon WHERE name = ?", @name)
+	end
+
+	def self.save(name, type, db)
+    	 db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  	end
+
+		def self.find(id, db)
+			#@sql_runner.execute_create_hp_column
+			name = db.execute("SELECT name FROM pokemon WHERE (id = (?)) LIMIT 1", id)
+			name = name.to_s.tr('[]\"', '')
+			type = db.execute("SELECT type FROM pokemon WHERE (id = (?)) LIMIT 1", id)
+			type = type.to_s.tr('[]\"', '')
+			#hp = db.execute("SELECT hp FROM pokemon WHERE (id = (?)) LIMIT 1", id)
+			#hp = type.to_s.tr('[]\"', '').to_i
+			poke = Pokemon.new(name, type)
+			poke.id = id
+			#poke.hp = 60
+			poke
+		end
+
+		def alter_hp(new_hp, db)
+			db.execute("UPDATE pokemon SET hp = (?) WHERE (name = (?))", new_hp, self.name)
+			@hp = new_hp
+		end
+
 end
