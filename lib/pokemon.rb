2aebb26c4a443db783b03598364d71b4eb73fb8c
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-11-08 11:44:48 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ class Pokemon
 
   def alter_hp(new_hp, db)
     @hp = new_hp
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, @id)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 
 end
